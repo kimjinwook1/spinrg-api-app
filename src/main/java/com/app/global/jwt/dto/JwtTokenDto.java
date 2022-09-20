91b0080af3e515e24bf90ccbb740a4ce41de8c25
kimjinwook1 <rlawlsdnr628@naver.com>
2022-09-20 22:50:11 +0900

    소셜 로그인 - TokenManger 구현 확인

--- src/main/java/com/app/global/jwt/dto/JwtTokenDto.java
+++ src/main/java/com/app/global/jwt/dto/JwtTokenDto.java
@@ -1,5 +1,7 @@
 package com.app.global.jwt.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
 import java.util.Date;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,8 +18,10 @@ public class JwtTokenDto {
 
 	private String grantType;
 	private String accessToken;
+	@JsonFormat(shape = Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
 	private Date accessTokenExpireTime;
 	private String refreshToken;
+	@JsonFormat(shape = Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
 	private Date refreshTokenExpireTime;
 
 }
