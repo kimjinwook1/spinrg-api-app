2adc17bbb2cb0138279b9a3a1821c7e58d32c1f2
kimjinwook1 <rlawlsdnr628@naver.com>
2022-09-23 22:28:59 +0900

    빈 주입 관련 설명

--- src/main/java/com/app/external/oauth/service/SocialLoginApiServiceFactory.java
+++ src/main/java/com/app/external/oauth/service/SocialLoginApiServiceFactory.java
@@ -11,6 +11,10 @@ public class SocialLoginApiServiceFactory {
 
 	public SocialLoginApiServiceFactory(Map<String, SocialLoginApiService> socialLoginApiServices) {
 		//Map 의 key값으로 bean의 이름, value에는 소셜로그인 api서비스의 구현체가 들어간다.
+		//SocialLoginApiService의 구현체들이 먼저 빈으로 등록되고
+		//그 다음에 SocialLoginApiServiceFactory에 해당 빈들을 주입해서 SocialLoginApiServiceFactory도 빈으로 등록을 해준다.
+		//스프링이 빈 등록 및 다른 빈 주입해주는것도 자동으로 해주는것이죠
+		//@Service 어노테이션이 붙었기 떄문에 빈 등록, 빈 주입을 자동으로 해줌
 		this.socialLoginApiServices = socialLoginApiServices;
 	}
 
